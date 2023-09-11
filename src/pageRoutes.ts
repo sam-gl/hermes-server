8a8747214d7f2adc2b311eabcc37b5129947329b
Sam <91395755+sam-gl@users.noreply.github.com>
2023-09-11 19:54:19 +0100

    add error handling to unsubscribe ui

--- src/pageRoutes.ts
+++ src/pageRoutes.ts
@@ -1,8 +1,14 @@
 import { Express, Request, Response } from "express";
 
+const { ERROR_CONTACT_MAIL } = process.env;
+
 export default (app: Express) => {
   app.get("/manage", (req: Request, res: Response) => {
-    res.render("manage", { subscriptionID: req.query.id });
+    res.render("manage", {
+      subscriptionID: req.query.id,
+      error: req.query.error,
+      errorContactEmail: ERROR_CONTACT_MAIL
+    });
   });
 
   app.get("/manage/subscriptions", (req: Request, res: Response) => {
